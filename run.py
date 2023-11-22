cd44a7a36fb5ab8796373a3459101c43d2a006ac
dratyti22 <137050298+dratyti22@users.noreply.github.com>
2023-11-22 17:10:49 +0300

    выбор напиток зарегистрирован

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
 import logging
 import asyncio
-from core.handlers import basic, types
+from core.handlers import basic, types_chat
 from dotenv import load_dotenv
 import os
 from core.handlers import group_games, username
@@ -18,6 +18,8 @@ from core.handlers import in_mp
 from core.handlers import admin_changes_in_group
 # from config_reader import config
 from core.handlers import fsmfood
+from core.fsmcontext_pack import fsmdriks
+
 
 async def start_bot(bot: Bot):
     await get_command(bot)
@@ -45,23 +47,23 @@ async def main():
     dp.startup.register(start_bot)
     dp.shutdown.register(stop_bot)
 
-    dp.message.register(basic.get_help, Command('help'), flags={'chat_action': 'typing'})#os.getenv('main_chat_id')
+    dp.message.register(basic.get_help, Command('help'), flags={'chat_action': 'typing'})  # os.getenv('main_chat_id')
 
     dp.include_router(admin_changes_in_group.router)
     dp.include_router(in_mp.router)
     dp.include_router(technical_service.router)
     dp.include_router(basic.router)
-    dp.include_router(types.router)
+    dp.include_router(types_chat.router)
     dp.include_router(group_games.router)
     dp.include_router(username.router)
-    dp.include_router(fsmfood.router)
+    dp.include_routers(fsmfood.router, fsmdriks.router)
 
     # admins = await bot.get_chat_administrators(config.main_chat_id)
     # admin_ids = {admin.user.id for admin in admins}
 
     try:
         await bot.delete_webhook(drop_pending_updates=True)
-        await dp.start_polling(bot )#admins=admin_ids
+        await dp.start_polling(bot)  # admins=admin_ids
     finally:
         await bot.session.close()
 
